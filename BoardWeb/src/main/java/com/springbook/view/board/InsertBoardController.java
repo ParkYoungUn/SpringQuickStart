ea54d479ee366f0b4d2a774d6d4cace5256f5933
swit01 <swit01@DESKTOP-MBTOPGP>
2021-03-16 15:44:28 +0900

    어노테이션 기반 MVC 개발(Controller, RequestMapping(value="") )

--- BoardWeb/src/main/java/com/springbook/view/board/InsertBoardController.java
+++ BoardWeb/src/main/java/com/springbook/view/board/InsertBoardController.java
@@ -1,40 +1,39 @@
 package com.springbook.view.board;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
-//import com.springbook.view.controller.Controller;
 
-public class InsertBoardController implements Controller {
+@Controller
+public class InsertBoardController {
 
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
+	@RequestMapping(value="insertBoard.do")
+	public void insertBoard(BoardVO vo) {
 		System.out.println("InsertBoardController 글 등록 처리");
 		
-		// 1. 사용자 입력 정보 추출
-//		request.setCharacterEncoding("UTF-8");  // DispatcherServletd의 doPost()가 인코딩 처리해줌
-		String title = request.getParameter("title");
-		String writer = request.getParameter("writer");
-		String content = request.getParameter("content");
+//		// 1. 사용자 입력 정보 추출
+////		request.setCharacterEncoding("UTF-8");  // DispatcherServletd의 doPost()가 인코딩 처리해줌
+//		String title = request.getParameter("title");
+//		String writer = request.getParameter("writer");
+//		String content = request.getParameter("content");
 		
-		// 2. 데이터베이스 연동 처리
-		BoardVO vo = new BoardVO();
-		vo.setTitle(title);
-		vo.setWriter(writer);
-		vo.setContent(content);
+//		// 2. 데이터베이스 연동 처리
+//		BoardVO vo = new BoardVO();
+//		vo.setTitle(title);
+//		vo.setWriter(writer);
+//		vo.setContent(content);
 		
 		BoardDAO boardDAO = new BoardDAO();
 		boardDAO.insertBoard(vo);
 		
-		// 3. 화면 네비게이션
-		ModelAndView mav = new ModelAndView(); 
-		mav.setViewName("redirect:getBoardList.do");
-		return mav;
+//		// 3. 화면 네비게이션
+//		ModelAndView mav = new ModelAndView(); 
+//		mav.setViewName("redirect:getBoardList.do");
+//		return mav;
 	}
 
 }
